9800d4f28fa5ab75c3dba7068d4cca322b24292a
TyMill <81069997+TyMill@users.noreply.github.com>
2023-01-25 14:55:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ if calendar_value:
     current_time = start_time
     values = []
     while current_time <= end_time:
-        values.append(st.checkbox(current_time.strftime("%I:%M %p")))
+        values.append(st.checkbox(current_time.strftime("%H:%M")))
         current_time += timedelta(minutes=30)
-    selected_times = [current_time.strftime("%I:%M %p") for current_time, value in zip(time_range, values) if value]
+    selected_times = [current_time.strftime("%H:%M") for current_time, value in zip(time_range, values) if value]
 
 # filter dataframe by date and time range
 df_filtered = df[(df["date"] == calendar_value.strftime("%Y-%m-%d")) & (df["time"].isin([time.strftime("%H:%M") for time in values]))]
