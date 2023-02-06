ce18d7f632afe30099e04e9d3b0122366105c2d1
TyMill <81069997+TyMill@users.noreply.github.com>
2023-02-06 15:40:32 +0100

    Update cal_place.py

--- cal_place.py
+++ cal_place.py
@@ -2,20 +2,22 @@ import pandas as pd
 import streamlit as st
 
 # Create sample data frame
-df = pd.DataFrame({'Date': ['2022-01-01', '2022-01-02', '2022-01-03', '2022-01-04'],
-                   'Place': ['NY', 'LA', 'SF', 'NY'],
+df = pd.DataFrame({'Date': ['2022-01-01', '2022-01-01', '2022-01-02', '2022-01-02'],
+                   'Hour': [8, 12, 8, 12],
+                   'Place': ['A', 'A', 'B', 'B'],
                    'Value': [10, 20, 30, 40]})
 df['Date'] = pd.to_datetime(df['Date'])
 
-def filter_by_date_place(df, date, place):
-    return df[(df['Date'] == date) & (df['Place'] == place)]
+def filter_by_date_hour_place(df, date, hour, place):
+    return df[(df['Date'] == date) & (df['Hour'] == hour) & (df['Place'] == place)]
 
 def main():
     st.title("Calendar and Data")
     date = st.date_input("Select Date", value=df['Date'].min())
-    place = st.selectbox("Select Place", df['Place'].unique())
-    filtered_df = filter_by_date_place(df, date, place)
-    st.write("Data for selected date and place:")
+    hour = st.selectbox("Select Hour", [8, 12])
+    place = st.selectbox("Select Place", ['A', 'B'])
+    filtered_df = filter_by_date_hour_place(df, date, hour, place)
+    st.write("Data for selected date, hour, and place:")
     st.write(filtered_df)
 
 if __name__ == '__main__':
